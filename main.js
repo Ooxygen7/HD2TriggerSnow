dc724ca28e25d237b71a38cf085fbfd2f551c844
OoxygenS7 <sdvvvcd@outlook.com>
2026-03-17 22:34:55 +0800

    V1.3.1_fix

--- main.js
+++ main.js
@@ -11,30 +11,60 @@ let isQuitting = false;
 
 keyboard.config.autoDelayMs = 0; 
 
+
 const uioToChar = {
-    30: "A", 48: "B", 46: "C", 32: "D", 18: "E", 33: "F", 34: "G", 35: "H",
-    23: "I", 36: "J", 37: "K", 38: "L", 50: "M", 49: "N", 24: "O", 25: "P",
-    16: "Q", 19: "R", 31: "S", 20: "T", 22: "U", 47: "V", 17: "W", 45: "X",
-    21: "Y", 44: "Z",
+
+    30: "KeyA", 48: "KeyB", 46: "KeyC", 32: "KeyD", 18: "KeyE", 33: "KeyF", 34: "KeyG", 35: "KeyH",
+    23: "KeyI", 36: "KeyJ", 37: "KeyK", 38: "KeyL", 50: "KeyM", 49: "KeyN", 24: "KeyO", 25: "KeyP",
+    16: "KeyQ", 19: "KeyR", 31: "KeyS", 20: "KeyT", 22: "KeyU", 47: "KeyV", 17: "KeyW", 45: "KeyX",
+    21: "KeyY", 44: "KeyZ",
+
     2: "Digit1", 3: "Digit2", 4: "Digit3", 5: "Digit4", 6: "Digit5", 
     7: "Digit6", 8: "Digit7", 9: "Digit8", 10: "Digit9", 11: "Digit0",
+
     82: "Numpad0", 79: "Numpad1", 80: "Numpad2", 81: "Numpad3", 75: "Numpad4",
     76: "Numpad5", 77: "Numpad6", 71: "Numpad7", 72: "Numpad8", 73: "Numpad9",
+
+    74: "NumpadSubtract", 78: "NumpadAdd", 55: "NumpadMultiply", 3653: "NumpadDivide", 83: "NumpadDecimal", 3612: "NumpadEnter",
+    98: "NumpadDivide",
+
     59: "F1", 60: "F2", 61: "F3", 62: "F4", 63: "F5", 64: "F6",
     65: "F7", 66: "F8", 67: "F9", 68: "F10", 87: "F11", 88: "F12",
-    57: "Space", 15: "Tab", 58: "CapsLock",
-    42: "ShiftLeft", 29: "ControlLeft", 56: "AltLeft",
-    54: "ShiftRight", 97: "ControlRight", 100: "AltRight"
+
+    57: "Space", 15: "Tab", 58: "CapsLock", 1: "Escape", 14: "Backspace", 28: "Enter",
+    42: "ShiftLeft", 29: "ControlLeft", 56: "AltLeft", 3675: "MetaLeft",
+    54: "ShiftRight", 3613: "ControlRight", 3640: "AltRight", 3676: "MetaRight",
+    97: "ControlRight", 100: "AltRight",
+
+    12: "Minus", 13: "Equal", 26: "BracketLeft", 27: "BracketRight",
+    39: "Semicolon", 40: "Quote", 41: "Backquote", 43: "Backslash",
+    51: "Comma", 52: "Period", 53: "Slash",
+
+    57416: "ArrowUp", 57424: "ArrowDown", 57419: "ArrowLeft", 57421: "ArrowRight",
+    3665: "PageUp", 3666: "PageDown", 3655: "Home", 3663: "End", 3660: "Insert", 3667: "Delete"
 };
 
 const nutKeyMap = {
-    'W': Key.W, 'S': Key.S, 'A': Key.A, 'D': Key.D,
-    'Up': Key.Up, 'Down': Key.Down, 'Left': Key.Left, 'Right': Key.Right,
-    'ArrowUp': Key.Up, 'ArrowDown': Key.Down, 'ArrowLeft': Key.Left, 'ArrowRight': Key.Right,
+
     'ControlLeft': Key.LeftControl, 'ControlRight': Key.RightControl,
-    'AltLeft': Key.LeftAlt, 'AltRight': Key.RightAlt,
     'ShiftLeft': Key.LeftShift, 'ShiftRight': Key.RightShift,
-    'CapsLock': Key.CapsLock, 'Tab': Key.Tab, 'Space': Key.Space
+    'AltLeft': Key.LeftAlt, 'AltRight': Key.RightAlt,
+    'MetaLeft': Key.LeftSuper, 'MetaRight': Key.RightSuper, 'OSLeft': Key.LeftSuper, 'OSRight': Key.RightSuper,
+
+    'ArrowUp': Key.Up, 'ArrowDown': Key.Down, 'ArrowLeft': Key.Left, 'ArrowRight': Key.Right,
+    'Up': Key.Up, 'Down': Key.Down, 'Left': Key.Left, 'Right': Key.Right,
+
+    'NumpadAdd': Key.Add, 'NumpadSubtract': Key.Subtract, 'NumpadMultiply': Key.Multiply, 'NumpadDivide': Key.Divide,
+    'NumpadDecimal': Key.Decimal, 'NumpadEnter': Key.Enter, 
+
+    'Enter': Key.Enter, 'Escape': Key.Escape, 'Backspace': Key.Backspace, 
+    'Space': Key.Space, 'Tab': Key.Tab, 'CapsLock': Key.CapsLock,
+    'PageUp': Key.PageUp, 'PageDown': Key.PageDown, 'Home': Key.Home, 'End': Key.End, 
+    'Insert': Key.Insert, 'Delete': Key.Delete,
+
+    'Minus': Key.Minus, 'Equal': Key.Equal, 'BracketLeft': Key.LeftBracket, 'BracketRight': Key.RightBracket,
+    'Semicolon': Key.Semicolon, 'Quote': Key.Quote, 'Backquote': Key.Grave, 'Backslash': Key.Backslash,
+    'Comma': Key.Comma, 'Period': Key.Period, 'Slash': Key.Slash
 };
 
 function createWindow() {
@@ -104,7 +134,6 @@ app.whenReady().then(() => {
         }
         return null; 
     });
-
 
     ipcMain.on('save-data', (event, filename, data) => {
         const filePath = path.join(userDataPath, filename);
@@ -199,7 +228,27 @@ ipcMain.on('execute-macro', async (event, payload) => {
     
     if (!sequence || sequence.length === 0) return;
 
-    const mKey = nutKeyMap[menuKey] || Key[menuKey] || Key.LeftControl;
+    function parseNutKey(kStr) {
+        if (!kStr) return null;
+        
+        if (nutKeyMap[kStr]) return nutKeyMap[kStr];
+
+        if (/^F\d{1,2}$/.test(kStr)) return Key[kStr];
+
+        if (/^Numpad\d$/.test(kStr)) return Key[kStr.replace('Numpad', 'NumPad')];
+
+        if (/^Digit\d$/.test(kStr)) return Key[kStr.replace('Digit', 'Num')];
+
+        if (/^Key[A-Z]$/.test(kStr)) return Key[kStr.replace('Key', '')];
+
+        if (/^[a-zA-Z]$/.test(kStr)) return Key[kStr.toUpperCase()];
+        if (/^\d$/.test(kStr)) return Key[`Num${kStr}`];
+
+        return null;
+    }
+
+    const mKey = parseNutKey(menuKey) || Key.LeftControl;
+    
     const MENU_OPEN_DELAY = Math.max(1, parseInt(menuOpenDelay) || 150);
     const PRESS_DELAY = Math.max(1, parseInt(pressDelay) || 15);
     const INTERVAL_DELAY = Math.max(1, parseInt(intervalDelay) || 15);
@@ -219,7 +268,7 @@ ipcMain.on('execute-macro', async (event, payload) => {
         await delay(MENU_OPEN_DELAY); 
 
         for (const k of sequence) {
-            const pressKey = nutKeyMap[k] || Key[k];
+            const pressKey = parseNutKey(k);
             if(pressKey) {
                 await keyboard.pressKey(pressKey);
                 await delay(PRESS_DELAY); 
